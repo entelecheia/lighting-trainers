968a408ddb4ba76e066a91e9c3b160fe6db630b3
Young Joon Lee <yj.lee@chu.ac.kr>
2021-06-08 00:01:13 +0000

    add korean spacing lightening transformer routines

--- run_models.sh
+++ run_models.sh
@@ -1,7 +1,8 @@
 python train.py \
     task=nlp/ekonspacing \
     dataset=nlp/token_classification/ekonspacing_small \
-    trainer=gpus \
+    trainer.gpus=1 \
+    trainer.max_epochs=4 \
     training.batch_size=32 \
-    training.num_workers=32 \
+    training.num_workers=16 \
     backbone.pretrained_model_name_or_path=entelecheia/ekonbert-base
